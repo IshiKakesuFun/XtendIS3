888bfeba2216e35f6f9392c32a1355f5501de6cd
PSojka <peter.sojka@anect.com>
2022-10-06 20:33:37 +0200

    db pool testing

--- utils/db_test.ts
+++ utils/db_test.ts
@@ -1,5 +1,38 @@
 import { assertEquals } from "testing/asserts.ts";
+import { pool } from "@db";
+import config from "@config";
 
-Deno.test("String a=a", () => {
-  assertEquals("a", "a");
+Deno.test("PostgresSQL database pool test", async (t) => {
+  await t.step("Pool available connections", () => {
+    assertEquals(pool.available, config.db.pool);
+  });
+
+  await t.step({
+    name: "Pool acquire connection",
+    fn: async () => {
+      const availableBefore = pool.available;
+      const client = await pool.connect();
+      assertEquals(pool.available, availableBefore - 1);
+      client.release();
+    },
+    ignore: false,
+    sanitizeOps: false,
+    sanitizeResources: false,
+    sanitizeExit: false,
+  });
+
+  await t.step({
+    name: "Pool release connection",
+    fn: async () => {
+      const client = await pool.connect();
+      const availableAfter = pool.available;
+      client.release();
+      assertEquals(pool.available, availableAfter + 1);
+    },
+    ignore: false,
+    sanitizeOps: false,
+    sanitizeResources: false,
+    sanitizeExit: false,
+  });
+  await pool.end();
 });
