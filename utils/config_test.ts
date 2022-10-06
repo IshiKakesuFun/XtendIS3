d7e104f97e7174bc81336bb1f119d2a30383a3b2
PSojka <peter.sojka@anect.com>
2022-10-06 21:26:59 +0200

    config test

---
+++ utils/config_test.ts
@@ -0,0 +1,11 @@
+import { assertEquals, assertExists } from "testing/asserts.ts";
+import { Environment } from "../interfaces/Config.ts";
+import config from "@config";
+
+Deno.test("Environment configuration", async (t) => {
+  await t.step("Deno Environment", () => {
+    assertExists(config);
+    assertEquals(config.environment, Deno.env.get("DENO_ENV") as Environment);
+  });
+
+});
